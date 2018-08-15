5f234772c4b161d06a78e8580c63eca220809370
Hassan Al-khalifah and Kate Goss <hassan@>
2018-08-15 16:49:40 -0700

    add vowel test method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var quay = "quay";
 var ay = "ay";
 var pigLatin = [];
-
+var vowelTest = function(word) {
+  return (/^[aeiou]$/).test(word);
+}
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
@@ -11,9 +13,10 @@ $(document).ready(function() {
     console.log(arrayWords);
 
     arrayWords.forEach(function(arrayWord) {
-      // if (arrayWord !== (/[\W])/g) {
-      //   return = false;
-      // }
+      var test = vowelTest(arrayWord[0]);
+      if( test == true)  {
+        console.log(test); // need to add "way" at end of word
+      }
       if (arrayWord.includes("qu")) {
         if(arrayWord[0] == "q" && arrayWord[1] == "u" && arrayWord !== ""  && arrayWord !== /\W/g ) {
           var slicedWord = arrayWord.slice(2);
