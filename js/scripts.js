2e3d66d77c211291c82a97b05627d7274d87a5c6
Hassan Al-khalifah and Kate Goss <hassan@>
2018-08-15 16:14:34 -0700

    add content to modify user input into pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
-var userInput;
+var quay = "quay";
+var ay = "ay";
+var pigLatin = [];
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    userInput = $("#userInput").val();
-    console.log(userInput);
+    $("#message").empty();
+    var userInput = $("#userInput").val();
+    var arrayWords = userInput.split(" ");
+    console.log(arrayWords);
+
+    arrayWords.forEach(function(arrayWord) {
+      // if (arrayWord !== (/[\W])/g) {
+      //   return = false;
+      // }
+      if (arrayWord.includes("qu")) {
+        if(arrayWord[0] == "q" && arrayWord[1] == "u" && arrayWord !== ""  && arrayWord !== /\W/g ) {
+          var slicedWord = arrayWord.slice(2);
+          console.log(slicedWord)
+          var quAtBegin = slicedWord.concat(quay);
+          pigLatin.push(quAtBegin);
+        } else {
+          var quAtMiddle = arrayWord.concat(ay);
+          pigLatin.push(quAtMiddle);
+        }
+      } else if (arrayWord !== "") {
+        var ifNoQu = arrayWord.concat(ay);
+        pigLatin.push(ifNoQu);
+      }
+    });
+    var finalPhrase = pigLatin.join(" ");
+    $("#message").text(finalPhrase);
   });
 });
